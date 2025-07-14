d048230bcf7f7eef6ffcb8057fb552e84a88c2ae
rifatahmed787 <mdrifatahmed787@gmail.com>
2025-07-14 11:49:12 +0600

    uselocalecontext

---
+++ src/hooks/useLocaleContext.ts
@@ -0,0 +1,13 @@
+
+import { ILocaleContextProps, LocaleContext } from "@/contexts/LocaleContext";
+import { useContext } from "react";
+
+export const useLocaleContext = (): ILocaleContextProps => {
+  const context = useContext(LocaleContext);
+  if (!context) {
+    throw new Error(
+      "useLocaleContext must be used within a LocaleContextProvider"
+    );
+  }
+  return context;
+};
